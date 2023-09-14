40ec76aa3a7d0ee9cb2c4ca6f0e6bda6149c7b5f
steadfasterX <steadfasterX@users.noreply.github.com>
2023-09-14 11:47:10 +0200

    fajita: add temp fscrypt patch

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+# DIRTY WORKAROUND to get FBEv2 working on fajita - until merged upstream
+TFILE=$PWD/out/fscrypt.patched
+[ ! -d "out" ]&& mkdir -p out
+RET=0
+
+if [ -f "$TFILE" ];then
+    echo "fscrypt patched already, skipping"
+else
+    cd system/extras
+    git apply ../../device/oneplus/fajita/patches/0001-fscrypt-bypass-FS_IOC_-G-S-ET_ENCRYPTION_POLICY.patch || RET=$?
+    if [ $RET -ne 0 ];then
+	echo "FATAL: fscrypt.cpp could not be patched!! Decryption in TWRP will FAIL."
+	exit $RET
+    else
+	echo "OK: fscrypt.cpp patched"
+	touch $TFILE
+    fi
+fi
